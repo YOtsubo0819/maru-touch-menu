2ea501dfce1f386d650c9ae5ec38ce8af74b3dfb
YOtsubo0819 <yojirootsubo@businessclinic.tokyo>
2021-09-21 12:10:21 +0900

    account

--- order/urls.py
+++ order/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'order'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('returnpt/input/', views.ReturninputView.as_view(), name='returninput'),
+    path('returninput/', views.ReturninputView.as_view(), name='returninput'),
     path('newinput/', views.NewinputView.as_view(), name='newinput'),
     path('gocounter/', views.GocounterView.as_view(), name='gocounter'),
     path('returnpt/', views.ReturnptView.as_view(), name='returnpt'),
